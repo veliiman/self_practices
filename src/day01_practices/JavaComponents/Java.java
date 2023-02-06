23933f30a10140d9eda8ad1a1f6f407849afcce2
veliiman <veliiman7051@gmail.com>
2023-02-06 19:21:21 +0100

    first commit

---
+++ src/day01_practices/JavaComponents/Java.java
@@ -0,0 +1,18 @@
+package day01_practices.JavaComponents;
+
+public class Java {
+
+    public static void main(String[] args) {
+
+        System.out.println("Java Development Kit");
+        System.out.println();
+        System.out.println("Java Runtime Environment");
+        System.out.println();
+        System.out.println("Java Virtual Machine");
+
+
+    }
+
+
+
+}
---
+++ src/day01_practices/MyClassmates.java
@@ -0,0 +1,16 @@
+package day01_practices;
+
+public class MyClassmates {
+
+    public static void main(String[] args) {
+
+        System.out.println("Fatih Cellek");
+        System.out.println("Yalcin Cakir");
+        System.out.println("Ardit Pemaj");
+        System.out.println("Burcu Kurukol");
+        System.out.println("Emre Ketenci");
+
+    }
+
+
+}
