001955cf51a5c115b099f326e1378f0609d42cc4
veliiman <veliiman7051@gmail.com>
2023-02-07 16:52:42 +0100

    first commit

---
+++ src/day02_practices/diamond2.java
@@ -0,0 +1,23 @@
+package day02_practices;
+
+public class diamond2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("      ^");
+        System.out.println("     / \\");
+        System.out.println("    / * \\");
+        System.out.println("   / * * \\");
+        System.out.println("  / * * * \\");
+        System.out.println("  ---------");
+        System.out.println("  \\ * * * /");
+        System.out.println("   \\ * * /");
+        System.out.println("    \\ * /");
+        System.out.println("     \\ /");
+        System.out.println("      v");
+
+
+    }
+
+
+}
