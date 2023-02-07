7cfcfbd38d009593ab295ea06e5e6431d8675066
veliiman <veliiman7051@gmail.com>
2023-02-07 16:40:55 +0100

    first commit

---
+++ src/day02_practices/Triangle.java
@@ -0,0 +1,35 @@
+package day02_practices;
+
+public class Triangle {
+
+    public static void main(String[] args) {
+
+        System.out.println("      ^");
+        System.out.println("     / \\");
+        System.out.println("    /   \\");
+        System.out.println("   /     \\");
+        System.out.println("  /       \\");
+        System.out.println("  ---------");
+
+
+    }
+
+
+}
+
+/*
+
+        System.out.println("----------------------------");
+
+        System.out.println("\tJava Programming Language");
+        System.out.println("\t\tJava Developper Course Batch 1");
+
+        System.out.println("\\");
+
+        System.out.println("----------------------------");
+
+        System.out.println("My favorite TV show is \"The Walking Dead\"");
+
+        System.out.println("\tMy Favorite book is \"The Rich Dad and The Poor Dad\"\n\tMy favorite quote is\"No Pain No Gain\"\n" +
+                "\tMy favorite TV show is \"The Game of Thrones\"\n\tMy favorite music is \"rock music\"");
+ */
